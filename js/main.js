0326a7d4d475a08f9482e877a2094cd26f0b5d83
troikas <dimiaggel@gmail.com>
2021-01-23 14:17:14 +0200

    Big changes on everything

--- js/main.js
+++ js/main.js
@@ -1,25 +1,45 @@
-function _(el) {
-  return document.getElementById(el);
+function _(e) {
+  return document.getElementById(e);
 }
 
-const gen = _('gen');
-gen.addEventListener('click', () => {
-    let x = document.querySelectorAll('.active');
-    if (x.length > 0) {
-      for (z = 0; z < x.length; z++) {
-        x[z].classList.remove('active');
-      }
+const myDay = new Date();
+let myYear = myDay.getFullYear();
+
+
+
+function makeTable(col, row, id) {
+  let myNum = 0;
+  let myTable = '';
+  for (i = 0; i < row; i++) {
+    myTable += '<tr>'
+    for (y = 0; y < col; y++) {
+      myNum += 1;
+      myTable += '<td ' + id + '-nu' + myNum + '">' + myNum + '</td>';
     }
-    for (y = 1; y <= 6; y++) {
-      let alln = gener();
-      for (i = 0; i < alln.nums.length; i++) {
-        let myn = _('tbn' + y + '-nu' + alln.nums[i]);
-        myn.classList.add('active');
-      }
-      let myj = _('tbj' + y + '-nj' + alln.jok);
-      myj.classList.add('active');
+    myTable += '</tr>';
+  }
+  return myTable;
+}
+
+let allBig = function() {
+    let allTables = '';
+    for (x = 0; x <= 5; x++) {
+      allTables += '<table class="tbn" id="tbn' + (x + 1) + '">';
+      allTables += makeTable(5, 9, 'id="tbn' + (x + 1));
+      allTables += '</table>';
     }
-  });
+    return allTables;
+  }
+
+let allJoker = function() {
+    let allTables = '';
+    for (x = 0; x <= 5; x++) {
+      allTables += '<table class="tbn" id="tbj' + (x + 1) + '">';
+      allTables += makeTable(5, 4, 'id="tbj' + (x + 1));
+      allTables += '</table>';
+    }
+    return allTables;
+  }
 
 function gener() {
   let allNums = {
@@ -43,8 +63,24 @@ function gener() {
   return allNums;
 }
 
-let myDay = new Date();
-let myYear = myDay.getFullYear();
+_('gen').addEventListener('click', () => {
+    let x = document.querySelectorAll('.active');
+    if (x.length > 0) {
+      for (z = 0; z < x.length; z++) {
+        x[z].classList.remove('active');
+      }
+    }
+    for (y = 1; y <= 6; y++) {
+      let alln = gener();
+      for (i = 0; i < alln.nums.length; i++) {
+        let myn = _('tbn' + y + '-nu' + alln.nums[i]);
+        myn.classList.add('active');
+      }
+      let myj = _('tbj' + y + '-nu' + alln.jok);
+      myj.classList.add('active');
+    }
+  });
 
-let copy = _('copy');
-copy.innerText = (myYear - 2) + '-' + (myYear + 2);
+_('table_big').innerHTML = allBig();
+_('table_joker').innerHTML = allJoker();
+_('copy').innerText = (myYear - 2) + '-' + (myYear + 2);
